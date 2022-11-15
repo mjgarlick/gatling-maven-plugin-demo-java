48dadb1fdb03d3c6345bfa01c5c481e9b9038665
Mike <mike@argodevops.co.uk>
2022-11-15 15:15:50 +0000

    Added assertions for gatling in jenkins

--- src/test/java/demosite/DemoLoginSite.java
+++ src/test/java/demosite/DemoLoginSite.java
@@ -24,10 +24,20 @@ public class DemoLoginSite extends Simulation {
     {
         switch(TYPE_OF_TEST) {
             case "RAMP_USERS":
-                setUp(PopulationTypes.rampUpUsers).protocols(httpProtocol);
+                setUp(PopulationTypes.rampUpUsers).protocols(httpProtocol)
+                        .assertions(
+                                global().responseTime().mean().lt(30),
+                                global().successfulRequests().percent().is(100.0),
+                                global().responseTime().max().lt(100)
+                        );
                 break;
             default:
-                setUp(PopulationTypes.immediateUsers).protocols(httpProtocol);
+                setUp(PopulationTypes.immediateUsers).protocols(httpProtocol)
+                        .assertions(
+                                global().responseTime().mean().lt(30),
+                                global().successfulRequests().percent().gt(99.9),
+                                global().responseTime().max().lt(100)
+                        );
                 break;
         }
     }
